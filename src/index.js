262f9a966777d29fb58ece594d476d7b56a86a3e
vitorhr10 <vitorrodrigueshr10@gmail.com>
2020-08-17 14:43:38 -0300

    routes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,37 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (resquest, response) => {
-    return response.json({ message: 'Hello dev' });
+app.get('/projects', (resquest, response) => {
+    return response.json([
+      'Projeto 1',
+      'Projeto 2',  
+    ]);
 });
+
+app.post('/projects', (resquest, response) => {
+    return response.json([
+      'Projeto 1',
+      'Projeto 2',
+      'Projeto 3',  
+    ]);
+});
+
+app.put('/projects', (resquest, response) => {
+    return response.json([
+      'Projeto 4',
+      'Projeto 2',
+      'Projeto 3',  
+    ]);
+});
+
+app.delete('/projects', (resquest, response) => {
+    return response.json([
+      'Projeto 2',
+      'Projeto 3',  
+    ]);
+});
+
+
+
 
 app.listen(3333);
