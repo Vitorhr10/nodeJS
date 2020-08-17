ad70cfceacb8ad72d2aca0c7e2cf6d90a2d234be
vitorhr10 <vitorrodrigueshr10@gmail.com>
2020-08-17 14:09:14 -0300

    http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+const express = require('express');
 
+const app = express();
+
+app.get('/', (resquest, response) => {
+    return response.json({ message: 'Hello dev' });
+});
+
+app.listen(3333);
